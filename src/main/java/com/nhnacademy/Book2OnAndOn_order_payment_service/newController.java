07dc090b6c489f6cda6babc4a54d57ecc3a51fb7
chye38 <dlrbgud38@gmail.com>
2025-11-16 17:43:14 +0900

    Feat: newController 생성

---
+++ src/main/java/com/nhnacademy/Book2OnAndOn_order_payment_service/newController.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.Book2OnAndOn_order_payment_service;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class newController {
+    //뭐를할까
+}
