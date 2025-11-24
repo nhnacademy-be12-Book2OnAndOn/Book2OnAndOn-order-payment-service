c4dd1c8b04a01b30c2c56c9eef15aabb41a82e4a
chye38 <dlrbgud38@gmail.com>
2025-11-24 19:11:14 +0900

    Feat: Payment 엔티티 수정

--- src/main/java/com/nhnacademy/Book2OnAndOn_order_payment_service/payment/domain/entity/Payment.java
+++ src/main/java/com/nhnacademy/Book2OnAndOn_order_payment_service/payment/domain/entity/Payment.java
@@ -23,40 +23,9 @@ public class Payment {
     @Column(name = "payment_id")
     private Long paymentId;
 
-    @Column(name = "payment_key", length = 200, unique = true)
-    @Size(max = 200)
-    private String paymentKey;
-
     @Column(name = "total_amount")
     @NotNull
     private Integer totalAmount;
-
-    @Column(name = "total_discount_amount")
-    @NotNull
-    private Integer totalDiscountAmount;
-
-    @Column(name = "total_item_amount")
-    @NotNull
-    private Integer totalItemAmount;
-
-    @Column(name = "delivery_fee")
-    @NotNull
-    private Integer deliveryFee;
-
-    @Column(name = "wrapping_fee")
-    @NotNull
-    private Integer wrappingFee;
-
-    @Column(name = "coupon_discount")
-    @NotNull
-    private Integer couponDiscount;
-
-    @Column(name = "point_discount")
-    @NotNull
-    private Integer pointDiscount;
-
-    @Column(name = "refund_amount")
-    private Integer refundAmount;
 
     @Column(name = "payment_method")
     @Size(max = 20)
@@ -79,6 +48,13 @@ public class Payment {
     @Column(name = "payment_receipt_url")
     @Size(max = 200)
     private String paymentReceiptUrl;
+
+    @Column(name = "payment_key", length = 200, unique = true)
+    @Size(max = 200)
+    private String paymentKey;
+
+    @Column(name = "refund_amount")
+    private Integer refundAmount;
 
     @Column(name = "order_id")
     @NotNull
