34282a83d35d494aa972226db0f46ea805e70f3e
chye38 <dlrbgud38@gmail.com>
2025-12-03 18:58:07 +0900

    Fix : 오류 제어 수정

--- src/main/java/com/nhnacademy/Book2OnAndOn_order_payment_service/order/exception/NotFoundOrderException.java
+++ src/main/java/com/nhnacademy/Book2OnAndOn_order_payment_service/order/exception/NotFoundOrderException.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.Book2OnAndOn_order_payment_service.order.exception;
 
-public class NotFoundOrderException extends RuntimeException {
+import com.nhnacademy.Book2OnAndOn_order_payment_service.exception.NotFoundException;
+
+public class NotFoundOrderException extends NotFoundException {
     public NotFoundOrderException(String message) {
         super(message);
     }
