ce26ccf041866c1bbf4fdd5bd4c49cc19e7bd776
김강민 <gangmin315@naver.com>
2025-12-04 19:28:06 +0900

    feat:포장지 service,controller 추가

--- src/main/java/com/nhnacademy/Book2OnAndOn_order_payment_service/order/repository/order/OrderItemRepository.java
+++ src/main/java/com/nhnacademy/Book2OnAndOn_order_payment_service/order/repository/order/OrderItemRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     List<OrderItem> findByOrder_OrderId(Long orderId);
+    long countByWrappingPaper_WrappingPaperId(Long wrappingPaperId);
 }
