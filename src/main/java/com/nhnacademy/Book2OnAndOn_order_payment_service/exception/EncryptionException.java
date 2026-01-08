81191a048e43aed5518e21b2fab937047d6b2b72
chye38 <dlrbgud38@gmail.com>
2026-01-08 20:47:37 +0900

    Issue : 작성

---
+++ src/main/java/com/nhnacademy/Book2OnAndOn_order_payment_service/exception/EncryptionException.java
@@ -0,0 +1,20 @@
+package com.nhnacademy.book2onandon_order_payment_service.exception;
+
+public class EncryptionException extends RuntimeException {
+
+    public EncryptionException() {
+        super();
+    }
+
+    public EncryptionException(String message) {
+        super(message);
+    }
+
+    public EncryptionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EncryptionException(Throwable cause) {
+        super(cause);
+    }
+}
